971389a0b22036215cce7f5873080b8482d124ae
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-07-03 15:38:49 +0200

    updated to ng8

--- src/lib/public_api.ts
+++
@@ -1,6 +0,0 @@
-export * from './module';
-export * from './dnd.config';
-export * from './draggable.component';
-export * from './droppable.component';
-export * from './sortable.component';
-export * from './dnd.service';
